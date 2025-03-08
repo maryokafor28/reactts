c05ff3992e2c0f94472fb50cdf6c5c684dadb398
maryokafor <vincyokafor@gmail.com>
2025-03-08 16:40:55 +0100

    joblisting

---
+++ reacttask/src/component/Joblist.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+type jobs = {
+  id: number;
+  title: string;
+  company: string;
+  location: string;
+  salary: string;
+};
+const Jobs: jobs[] = [
+  {
+    id: 1,
+    title: "frontend developer",
+    company: "techy",
+    location: "makurdi",
+    salary: "₦400,000 per month",
+  },
+
+  {
+    id: 2,
+    title: "Backend Engineer",
+    company: "hikuptech",
+    location: "portharcourt",
+    salary: "₦600,000 per month",
+  },
+  {
+    id: 3,
+    title: "Product Designer",
+    company: "techy",
+    location: "Abuja",
+    salary: "₦500,000 per month",
+  },
+  {
+    id: 4,
+    title: "UI/UX Designer",
+    company: "techy",
+    location: "Abuja",
+    salary: "₦300,000 per month",
+  },
+  {
+    id: 5,
+    title: "Data Analyst",
+    company: "mozilla",
+    location: "Enugu",
+    salary: "₦450,000 per month",
+  },
+  {
+    id: 6,
+    title: "Product Manager",
+    company: "hottech",
+    location: "Porthacourt",
+    salary: "₦550,000 per month",
+  },
+  {
+    id: 7,
+    title: "Frontend Developer",
+    company: "tech corps",
+    location: "Owerri",
+    salary: "₦450,000 per month",
+  },
+  {
+    id: 8,
+    title: "Fullstack Developer",
+    company: "Dev Corp",
+    location: "Lagos",
+    salary: "₦750,000 per month",
+  },
+  {
+    id: 8,
+    title: "Product Manager",
+    company: "Tech Devops",
+    location: "Porharcourt",
+    salary: "₦800,000 per month",
+  },
+  {
+    id: 9,
+    title: "Cybersecurity",
+    company: "Advan tech",
+    location: "Makurdi",
+    salary: "₦400,000 per month",
+  },
+  {
+    id: 10,
+    title: "Tech Manager",
+    company: "Hottech",
+    location: "Abuja",
+    salary: "₦550,000 per month",
+  },
+];
